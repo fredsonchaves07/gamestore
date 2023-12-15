29058a986cadbf6cd39665b2f2c38f14fec6c6f9
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-12-15 11:55:40 -0300

    feat: update logo platform nintendo 64
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

---
+++ src/main/resources/db/migration/V15__update_logo_n64.sql
@@ -0,0 +1,3 @@
+UPDATE platforms SET logo_url = '//images.igdb.com/igdb/image/upload/t_logo_med/pl77.png' WHERE id = 4;
+
+UPDATE platforms SET logo_url = '//images.igdb.com/igdb/image/upload/t_logo_med/plj8.png' WHERE id = 416;
